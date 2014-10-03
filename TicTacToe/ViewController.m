9ee7273dcedbb541440631331d46a7606f84a53c
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-03 13:38:20 -0500

    Beginning Project

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIView *labelOne;
+@property (strong, nonatomic) IBOutlet UILabel *labelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *labelThree;
+@property (strong, nonatomic) IBOutlet UILabel *labelFour;
+@property (strong, nonatomic) IBOutlet UILabel *labelFive;
+@property (strong, nonatomic) IBOutlet UILabel *labelSix;
+@property (strong, nonatomic) IBOutlet UILabel *labelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *labelEight;
+@property (strong, nonatomic) IBOutlet UILabel *labelNine;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
 @end
 
@@ -23,5 +33,28 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)findLabelUsing:(id)sender
+
+
+//-(IBAction)onDrag:(UIPanGestureRecognizer *) panGesture {
+//    CGPoint point = [panGesture locationInView:self.view];
+//    self.theFuture.center = point;
+//
+//    if (panGesture.state == UIGestureRecognizerStateEnded) {
+//        [UIView animateWithDuration:1.0 animations:^{
+//            self.theFuture.center = self.originalCenter;
+//            self.theFuture.backgroundColor = [UIColor greenColor];
+//
+//        }];
+//    }
+//
+//    if (CGRectContainsPoint(self.thePreCogs.frame, point)) {
+//        self.theFuture.backgroundColor = [UIColor orangeColor];
+//        self.theFuture.text = @"A Fictitious and Incriminating Future";
+//        [self.theFuture sizeToFit];
+//    }
+}
+
 
 @end
