abf3f0e5e39023e97ea00a5aaadbc9a6b357664c
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-05 23:11:04 -0500

    Finished Through 5

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -22,56 +22,43 @@
 @property (strong, nonatomic) IBOutlet UILabel *labelNine;
 @property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property (strong, nonatomic) IBOutlet UILabel *timerLabel;
+
 @property BOOL playerTurn;
-@property BOOL alreadyPlayed;
-@property NSArray *validMove;
-
-
-@property NSString *lastMove;
 @property NSArray *allLabels;
 @property int timeAmount;
 @property NSTimer *gameTimer;
 @property CGPoint gamePieceOriginalCenter;
-@property NSArray *columnsAndRows;
-@property UILabel *humanMove;
-@property UILabel *twoHumanMovesAgo;
-@property int moveNumber;
+@property NSArray *win;
 @property CGRect originalPosition;
 
 @end
 
 
-// Ruggles Tic Tac Toe
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.whichPlayerLabel.text = @"X";
-    self.whichPlayerLabel.textColor = [UIColor blueColor];
+    self.whichPlayerLabel.text = @"Your turn X";
+    self.whichPlayerLabel.textColor = [UIColor whiteColor];
     self.timerLabel.text = @"10";
     self.playerTurn = 0;
 
-
     self.allLabels = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
 
-    NSArray *validMove = [NSArray 0, 0, 0, 0, 0, 0, 0, 0, 0, nil];
+    NSArray *rowOne = @[self.labelOne, self.labelTwo, self.labelThree];
+    NSArray *rowTwo = @[self.labelFour, self.labelFive, self.labelSix];
+    NSArray *rowThree = @[self.labelSeven, self.labelEight, self.labelNine];
+    NSArray *columnOne = @[self.labelOne, self.labelFour, self.labelSeven];
+    NSArray *columnTwo = @[self.labelTwo, self.labelFive, self.labelEight];
+    NSArray *columnThree = @[self.labelThree, self.labelSix, self.labelNine];
+    NSArray *diagonalLeft = @[self.labelOne, self.labelFive, self.labelNine];
+    NSArray *diagonalRight = @[self.labelThree, self.labelFive, self.labelSeven];
 
-    NSArray *rowOne = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, nil];
-    NSArray *rowTwo = [NSArray arrayWithObjects:self.labelFour, self.labelFive, self.labelSix, nil];
-    NSArray *rowThree = [NSArray arrayWithObjects:self.labelSeven, self.labelEight, self.labelNine, nil];
-    NSArray *columnOne = [NSArray arrayWithObjects:self.labelOne, self.labelFour, self.labelSeven, nil];
-    NSArray *columnTwo = [NSArray arrayWithObjects:self.labelTwo, self.labelFive, self.labelEight, nil];
-    NSArray *columnThree = [NSArray arrayWithObjects:self.labelThree, self.labelSix, self.labelNine, nil];
-    NSArray *diagonalLeft = [NSArray arrayWithObjects:self.labelOne, self.labelFive, self.labelNine, nil];
-    NSArray *diagonalRight = [NSArray arrayWithObjects:self.labelThree, self.labelFive, self.labelSeven, nil];
-
-    self.columnsAndRows = [NSArray arrayWithObjects:rowOne, rowTwo, rowThree, columnOne, columnTwo, columnThree, diagonalLeft, diagonalRight,  nil];
+    self.win = @[rowOne, rowTwo, rowThree, columnOne, columnTwo, columnThree, diagonalLeft, diagonalRight];
 
     self.gamePieceOriginalCenter = self.whichPlayerLabel.center;
-
 }
-
 
 -(UILabel *)findLabelUsingPoint: (CGPoint)point {
     for (UILabel *label in self.allLabels) {
@@ -84,24 +71,62 @@
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)userTap {
     CGPoint point = [userTap locationInView:self.view];
-    UILabel *tappedLabel = [self findLabelUsingPoint:point];
-    if (tappedLabel) {
-        if (self.tappedlabel [tag] == 0) {
+    UILabel *theTappedLabelIfItExists = [self findLabelUsingPoint:point];
+    if (theTappedLabelIfItExists) {
+        // if there is an X or an O, then it's been tapped, if not, it's empty
+
+        BOOL theLabelHasNotBeenTapped = [theTappedLabelIfItExists.text isEqualToString:@""];
+
+        if (theLabelHasNotBeenTapped) {
             if (self.playerTurn == 0) {
-                tappedLabel.text = @"X";
-                tappedLabel.textColor = [UIColor blueColor];
+                theTappedLabelIfItExists.text = @"X";
+                theTappedLabelIfItExists.textColor = [UIColor blueColor];
                 self.whichPlayerLabel.text = @"Your turn O";
             }
             else {
-                tappedLabel.text = @"O";
-                tappedLabel.textColor = [UIColor redColor];
+                theTappedLabelIfItExists.text = @"O";
+                theTappedLabelIfItExists.textColor = [UIColor redColor];
                 self.whichPlayerLabel.text = @"Your turn X";
             }
-            self.playerTurn = !self.playerTurn;
-            self.validMove[] = 1;
-            
+
+            NSString *theWinner = [self whoWon];
+            if ([theWinner isEqual:@"X won"]) {
+                // show an alert view announcing the winner
+                UIAlertView *alertView = [[UIAlertView alloc]init];
+                alertView.delegate = self;
+                alertView.title = @"X Wins!";
+                [alertView addButtonWithTitle: @"Play Again"];
+                [alertView show];
+            }
+            else if ([theWinner isEqual:@"O won"]) {
+                UIAlertView *alertView = [[UIAlertView alloc]init];
+                alertView.delegate = self;
+                alertView.title = @"O Wins!";
+                [alertView addButtonWithTitle: @"Play Again"];
+                [alertView show];
+
+            } else {
+                self.playerTurn = !self.playerTurn;
+            }
         }
     }
+}
+
+- (NSString *) whoWon {
+    for (NSArray *labels in self.win) {
+
+        NSArray *textValues = [labels valueForKey:@"text"];
+
+        NSString *joinedValues = [textValues componentsJoinedByString:@""];
+        if ([joinedValues  isEqual: @"XXX"])  {
+            return @"X won";
+        }
+
+        if ([joinedValues isEqual:@"OOO"]) {
+            return @"O won";
+        }
+    }
+    return @"";
 }
 
 @end
